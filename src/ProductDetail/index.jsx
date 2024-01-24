661ffb24bd4c8de76b9645b4c7e6133f6835bf1b
QulamSamadli <qulam999@gmail.com>
2024-01-25 02:58:01 +0400

    added codes

--- src/ProductDetail/index.jsx
+++ src/ProductDetail/index.jsx
@@ -2,21 +2,22 @@ import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router";
 
-const { id } = useParams();
-const _url = "https://dummyjson.com/products/";
 
 const ProductDetail = () => {
-  const [detail, setDetail] = useState([]);
-  useEffect(() => {
-    axios.get(_url + id).then((data) => {
-      setDetail(data);
-    });
-  }, []);
+    const { id } = useParams();
+    const _url = "https://dummyjson.com/products/"+id;
+    const [detail, setDetail] = useState([]);
+    useEffect(() => {
+        axios.get(_url ).then((data) => {
+            setDetail(data);
+            console.log(data);
+        });
+  }, [id]);
   return (
-    <div>
-      <h2>{data.description}</h2>
-      <p>{data.price}</p>
-      <img src={data.images[1]} alt="" />
+    <div key={id}>
+      <h2>{detail.description}</h2>
+      <p>{detail.price}</p>
+      <img src={detail.images[0]} alt="" />
     </div>
   );
 };
--- src/ProductsCard/index.jsx
+++ src/ProductsCard/index.jsx
@@ -8,6 +8,7 @@ const ProductsCard = ({title,brand,images,id}) => {
         <h1>{brand}</h1>
         <p>{title}</p>
       <div className='w-[200px] h-[275px]'>  <img src={images[0]} alt="" /></div>
+    
     </Link>
   )
 }
