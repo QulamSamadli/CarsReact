27bbaa85f50ab3265c80772d84b87107722273b8
QulamSamadli <qulam999@gmail.com>
2024-01-25 18:41:36 +0400

    changes classname

--- src/ProductsCard/index.jsx
+++ src/ProductsCard/index.jsx
@@ -3,16 +3,28 @@ import { Link } from "react-router-dom";
 
 const ProductsCard = ({ title, brand, images, id }) => {
   return (
-    <Link to={`/products/${id}`} className="border-2 rounded-md border-[#CDC7E5] w-[250px] h-[200px] grid grid-cols-2 text-[#6457A6] p-4 bg-[#white]">
-      <div className="flex flex-col">
-        <h1>{brand}</h1>
-        <p>{title}</p>
-      </div>
-      <div className="w-[100px] h-[280px]">
-        <img src={images[0]} alt="" />
+    <Link
+      to={`/products/${id}`}
+      className=""
+    >
+      <div className="flex flex-col items-center border-2 rounded-md border-[#CDC7E5] w-[370px] m-4 p-4 h-[320px] text-[#6457A6] bg-[#white]">
+        <div className="w-[200px] h-[2000px]" ><img src={images[0]} alt="" /></div>
+        <div className=""><h2>{brand} {title}</h2></div>
       </div>
     </Link>
   );
 };
 
 export default ProductsCard;
+
+
+{/* <div className="flex gap-4">
+        {" "}
+        <div className="flex flex-col gap-6 text-[18px]">
+          <h1>Brand:{brand}</h1>
+          <p>Model:{title}</p>
+        </div>
+        <div className="w-[100px] h-[280px]">
+          <img src={images[0]} alt="" />
+        </div>
+      </div> */}
